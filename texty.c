a053487f7ea861fcd6eaa776a26feec9309e2ba5
OmKharade <133861833+OmKharade@users.noreply.github.com>
2025-05-08 20:18:28 -0400

    Vertical scrolling - using `rowoff` offset to simulate scrolling

--- texty.c
+++ texty.c
@@ -41,6 +41,7 @@ struct editorConfig{
   int screencols;
   int numrows;
   erow *row;
+  int rowoff;
   struct termios default_termios;
 };
 
@@ -234,7 +235,7 @@ void editorMoveCursor(int key){
       E.cy--;
     break;
   case ARROW_DOWN:
-    if(E.cy != E.screenrows - 1)
+    if(E.cy < E.numrows)
       E.cy++;
     break;
   }
@@ -275,9 +276,19 @@ void editorProcessKeypresses(){
 
 /* output */
 
+void editorScroll(){
+  if(E.cy < E.rowoff){
+    E.rowoff = E.cy;
+  }
+  if(E.cy >= E.rowoff + E.screenrows){
+    E.rowoff = E.cy - E.screenrows + 1;
+  }
+}
+
 void editorDrawRows(struct abuf *ab){
   for(int y = 0; y < E.screenrows; y++){
-    if(y >= E.numrows){
+    int filerow = y + E.rowoff;
+    if(filerow >= E.numrows){
       if(E.numrows == 0 && y == E.screenrows/3){
         char welcome[80];
         int welcomelen = snprintf(welcome, sizeof(welcome), "TEXT EDITOR -- version %s", TEXTY_VERSION);
@@ -294,9 +305,9 @@ void editorDrawRows(struct abuf *ab){
       }
     }
     else{
-      int len = E.row[y].size;
+      int len = E.row[filerow].size;
       if(len > E.screencols) len = E.screencols;
-        abAppend(ab, E.row[y].chars, len);
+        abAppend(ab, E.row[filerow].chars, len);
     }
     abAppend(ab, "\x1b[K", 3);
     if(y < E.screenrows-1)
@@ -305,6 +316,8 @@ void editorDrawRows(struct abuf *ab){
 }
 
 void editorRefreshScreen(){
+  editorScroll();
+
   struct abuf ab = ABUF_INIT;
 
   abAppend(&ab, "\x1b[?25l", 6);
@@ -313,7 +326,7 @@ void editorRefreshScreen(){
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy+1, E.cx+1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy-E.rowoff+1, E.cx+1);
   abAppend(&ab, buf, strlen(buf));
   abAppend(&ab, "\x1b[?25h", 6);
 
@@ -328,6 +341,7 @@ void initEditor(){
   E.cy = 0;
   E.numrows = 0;
   E.row = NULL;
+  E.rowoff = 0;
 
   if(getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSize");
