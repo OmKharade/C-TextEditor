2ae30efcbecbb9d96b240e37405607c9c75b4403
OmKharade <133861833+OmKharade@users.noreply.github.com>
2025-05-08 20:03:48 -0400

    Read multiple lines

--- texty.c
+++ texty.c
@@ -40,7 +40,7 @@ struct editorConfig{
   int screenrows;
   int screencols;
   int numrows;
-  erow row;
+  erow *row;
   struct termios default_termios;
 };
 
@@ -165,6 +165,19 @@ int getWindowSize(int *rows, int *cols){
   }
 }
 
+/* row operations */
+
+void editorAppendRow(char *s, size_t len){
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows+1));
+
+  int at = E.numrows;
+  E.row[at].size = len;
+  E.row[at].chars = malloc(len + 1);
+  memcpy(E.row[at].chars, s, len);
+  E.row[at].chars[len] = '\0';
+  E.numrows++;
+}
+
 /* file i/o */
 
 void editorOpen(char *filename){
@@ -174,17 +187,11 @@ void editorOpen(char *filename){
   char *line = NULL;
   size_t linecap = 0;
   ssize_t linelen;
-  linelen = getline(&line, &linecap, fp);
-  if(linelen != -1){
+  while((linelen = getline(&line, &linecap, fp)) != -1){
     while(linelen > 0 && (line[linelen-1] == '\n' || line[linelen-1] == '\r'))
       linelen--;
-      
-    E.row.size = linelen;
-    E.row.chars = malloc(linelen + 1);
-    memcpy(E.row.chars, line, linelen);
-    E.row.chars[linelen] = '\0';
-    E.numrows = 1;
-  }
+    editorAppendRow(line, linelen);
+    }
   free(line);
   fclose(fp);
 }
@@ -287,9 +294,9 @@ void editorDrawRows(struct abuf *ab){
       }
     }
     else{
-      int len = E.row.size;
+      int len = E.row[y].size;
       if(len > E.screencols) len = E.screencols;
-        abAppend(ab, E.row.chars, len);
+        abAppend(ab, E.row[y].chars, len);
     }
     abAppend(ab, "\x1b[K", 3);
     if(y < E.screenrows-1)
@@ -320,6 +327,8 @@ void initEditor(){
   E.cx = 0;
   E.cy = 0;
   E.numrows = 0;
+  E.row = NULL;
+
   if(getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSize");
 }
